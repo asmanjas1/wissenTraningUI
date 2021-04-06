28f555ede57f2615a1b59803013ecc4830acd9aa
asmanjas <asmanjas.singh@matrixres.com>
2021-04-06 06:30:04 -0400

    First commit for my react app from asmanjas

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NotFound from './pages/commonComponents/NotFound.js';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
+    <div>
+
+            <div className="main-cls" style={{minWidth: '1100px'}}>
+                <div>
+                    <Switch>
+                        <Route path="/r" component={App}/>
+                        <Route exact path="/" render={() => (
+                            <Redirect to="/r"/>
+                        )}/>
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </div>
+            </div>
+
+        </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ src/pages/commonComponents/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound = () => (
+<div className="not-found-body-class" id='notFoundId'>
+	No Pages Found
+</div>
+);
+export default NotFound;
